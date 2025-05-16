a95e16b8a5a2e2aa31abe68bc6576a86a4e1ec1b
sairaj <sairajt70@gmail.com>
2025-05-16 14:31:23 +0530

    fixed cors issues

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from .models import Base
 from .database import engine
 from .chat import router as chat_router
 from .session import router as session_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -12,3 +14,11 @@ Base.metadata.create_all(bind=engine)
 # Register API routes
 app.include_router(chat_router)
 app.include_router(session_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
